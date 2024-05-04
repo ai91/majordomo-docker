7f60ac6e1fadaf166fc294baf69555ba501f65ec
Anar Ibragimoff <anar.ibragimoff@gmail.com>
2024-05-04 03:01:36 +0200

    initialize db_terminal on new installations

--- db_terminal_init.php
+++ db_terminal_init.php
@@ -7,5 +7,12 @@ $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 if(!$mysqli->query("DESCRIBE settings")) {
 	$sql = file_get_contents('db_terminal.sql');
-	$mysqli->multi_query($sql);
+	if ($mysqli->multi_query($sql)) {
+		do {
+			if ($result = $mysqli->store_result()) {
+				$result->free();
+			}
+		} while ($mysqli->more_results() && $mysqli->next_result());
+	}
 }
+$mysqli->close();
