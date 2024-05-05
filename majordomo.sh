ef0fb0e9adf8d29f44b41633692d19e3375f1766
Anar Ibragimoff <anar.ibragimoff@gmail.com>
2024-05-05 22:08:47 +0200

    owner&permissions on default folders

--- majordomo.sh
+++ majordomo.sh
@@ -5,8 +5,8 @@ if [ -z "$(ls -A /var/www/html/cms)" ]; then
     # If empty, copy contents from /var/www/html/cms.default
     echo "cms directory is empty. Copying default content"
     cp -r /var/www/html/cms.default/* /var/www/html/cms/
-    find /var/www/html/cms/ -type d -exec chmod 777 {} && chown www-data:www-data {} \;
-    find /var/www/html/cms/ -type f -exec chmod 666 {} && chown www-data:www-data {} \;
+    find /var/www/html/cms/ -type d -exec /bin/sh -c "chmod 777 {} && chown www-data:www-data {}" \;
+    find /var/www/html/cms/ -type f -exec /bin/sh -c "chmod 666 {} && chown www-data:www-data {}" \;
 fi
 
 # check if /var/www/html/modules is empty
@@ -14,8 +14,8 @@ if [ -z "$(ls -A /var/www/html/modules)" ]; then
     # If empty, copy contents from /var/www/html/modules.default
     echo "modules directory is empty. Copying default content"
     cp -r /var/www/html/modules.default/* /var/www/html/modules/
-    find /var/www/html/modules/ -type d -exec chmod 777 {} && chown www-data:www-data {} \;
-    find /var/www/html/modules/ -type f -exec chmod 666 {} && chown www-data:www-data {} \;
+    find /var/www/html/modules/ -type d -exec /bin/sh -c "chmod 777 {} && chown www-data:www-data {}" \;
+    find /var/www/html/modules/ -type f -exec /bin/sh -c "chmod 666 {} && chown www-data:www-data {}" \;
 fi
 
 export -p > /root/env.sh
