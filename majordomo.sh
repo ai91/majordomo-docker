87794bf91042110867b417cd15955e35628dbb00
Anar Ibragimoff <anar.ibragimoff@gmail.com>
2024-05-05 20:38:46 +0200

    add default modules

--- majordomo.sh
+++ majordomo.sh
@@ -5,6 +5,17 @@ if [ -z "$(ls -A /var/www/html/cms)" ]; then
     # If empty, copy contents from /var/www/html/cms.default
     echo "cms directory is empty. Copying default content"
     cp -r /var/www/html/cms.default/* /var/www/html/cms/
+    find /var/www/html/cms/ -type d -exec chmod 777 {} && chown www-data:www-data {} \;
+    find /var/www/html/cms/ -type f -exec chmod 666 {} && chown www-data:www-data {} \;
+fi
+
+# check if /var/www/html/modules is empty
+if [ -z "$(ls -A /var/www/html/modules)" ]; then
+    # If empty, copy contents from /var/www/html/modules.default
+    echo "modules directory is empty. Copying default content"
+    cp -r /var/www/html/modules.default/* /var/www/html/modules/
+    find /var/www/html/modules/ -type d -exec chmod 777 {} && chown www-data:www-data {} \;
+    find /var/www/html/modules/ -type f -exec chmod 666 {} && chown www-data:www-data {} \;
 fi
 
 export -p > /root/env.sh
