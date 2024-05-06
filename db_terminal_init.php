0e32d41d590f227ca5e79a973494c23042e739e6
Anar Ibragimoff <anar.ibragimoff@gmail.com>
2024-05-06 22:33:25 +0200

    db_terminal initialization error handling

--- db_terminal_init.php
+++ db_terminal_init.php
@@ -6,6 +6,7 @@
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 if(!$mysqli->query("DESCRIBE settings")) {
+	echo "Initializing database with db_terminal.sql content..." . PHP_EOL;
 	$sql = file_get_contents('db_terminal.sql');
 	if ($mysqli->multi_query($sql)) {
 		do {
@@ -13,6 +14,12 @@ if(!$mysqli->query("DESCRIBE settings")) {
 				$result->free();
 			}
 		} while ($mysqli->more_results() && $mysqli->next_result());
+	}
+	if ($mysqli->errno) {
+		echo "Initialization failed." . PHP_EOL;
+		var_dump($mysqli->error);
+	} else {
+		echo "Initializing database done" . PHP_EOL;
 	}
 }
 $mysqli->close();
