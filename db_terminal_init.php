89c18c0e3259f36b57b08669405d3721da71869d
Anar Ibragimoff <anar.ibragimoff@gmail.com>
2024-05-08 07:33:47 +0200

    move db_terminal initialization to entrypoint

--- db_terminal_init.php
+++ db_terminal_init.php
@@ -7,25 +7,50 @@ chdir(dirname(__FILE__));
 
 include_once("./config.php");
 
+// temporary for debugging purposes. TODO remove this echoes
 echo "DB_HOST = " . DB_HOST . PHP_EOL;
+echo "DB_USER = " . DB_USER . PHP_EOL;
+echo "DB_PASSWORD = " . DB_PASSWORD . PHP_EOL;
+echo "DB_NAME = " . DB_NAME . PHP_EOL;
 
-$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+$retryInterval = 5;
+$maxRetries = 10;
+$retryCount = 0;
 
-if(!$mysqli->query("DESCRIBE classes")) {
-	echo "Initializing database with db_terminal.sql content..." . PHP_EOL;
-	$sql = file_get_contents('db_terminal.sql');
-	if ($mysqli->multi_query($sql)) {
-		do {
-			if ($result = $mysqli->store_result()) {
-				$result->free();
-			}
-		} while ($mysqli->more_results() && $mysqli->next_result());
-	}
-	if ($mysqli->errno) {
-		echo "Initialization failed." . PHP_EOL;
-		var_dump($mysqli->error);
+echo "Initializing database with db_terminal.sql content..." . PHP_EOL;
+while ($retryCount < $maxRetries) {
+	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	if ($mysqli->connect_error) {
+		echo "Connection failed: " . $mysqli->connect_error . "\n";
+		echo "Retrying in $retryInterval seconds...\n";
+		sleep($retryInterval);
+		$retryCount++;
 	} else {
-		echo "Initializing database done" . PHP_EOL;
+		echo "Connected successfully!\n";
+		if(!$mysqli->query("DESCRIBE classes")) {
+			$sql = file_get_contents('db_terminal.sql');
+			if ($mysqli->multi_query($sql)) {
+				do {
+					if ($result = $mysqli->store_result()) {
+						$result->free();
+					}
+				} while ($mysqli->more_results() && $mysqli->next_result());
+			}
+			if ($mysqli->errno) {
+				echo "Initialization failed." . PHP_EOL;
+				var_dump($mysqli->error);
+			} else {
+				echo "Initializing database done" . PHP_EOL;
+			}
+		} else {
+			echo "Database is good, skipping." . PHP_EOL;
+		}
+		$mysqli->close();
+		break;
 	}
 }
-$mysqli->close();
+
+if ($retryCount === $maxRetries) {
+	echo "Unable to connect to the database after $maxRetries retries. Exiting...\n";
+	exit(1);
+}
