a883a620ee3f8ecdfd532e40f34fc54ace5b17fa
Anar Ibragimoff <anar.ibragimoff@gmail.com>
2024-05-09 18:32:24 +0200

    $MAJORDOMO_DONT_RESTORE_FILES & $MAJORDOMO_DONT_RESTORE_FILES_FILE support

--- majordomo.sh
+++ majordomo.sh
@@ -9,23 +9,11 @@ if [ -n "$MAJORDOMO_DONT_RESTORE_FILES" ]; then
   echo "$MAJORDOMO_DONT_RESTORE_FILES" | tr ',' '\n' >> /tmp/excludes
 fi
 
-# append default content from cms
-echo "Appending default content of cms directory."
-rsync -a --exclude-from=/tmp/excludes /var/www/html/cms.default/ /var/www/html/cms/
+# append default content from cms/modules/scripts
+echo "Appending default content of cms/modules/scripts directories."
+rsync -a --exclude-from=/tmp/excludes /var/www/html/default_distribution/ /var/www/html/
 find /var/www/html/cms/ -type d -exec /bin/sh -c "chmod 777 {} && chown www-data:www-data {}" \;
 find /var/www/html/cms/ -type f -exec /bin/sh -c "chmod 666 {} && chown www-data:www-data {}" \;
-
-# append default content from modules
-echo "Appending default content of modules directory."
-rsync -a --exclude-from=/tmp/excludes /var/www/html/modules.default/ /var/www/html/modules/
-find /var/www/html/modules/ -type d -exec /bin/sh -c "chmod 777 {} && chown www-data:www-data {}" \;
-find /var/www/html/modules/ -type f -exec /bin/sh -c "chmod 666 {} && chown www-data:www-data {}" \;
-
-# append default content from scripts
-rsync -a --exclude-from=/tmp/excludes /var/www/html/scripts.default/ /var/www/html/scripts/
-echo "Appending default content of scripts directory."
-find /var/www/html/scripts/ -type d -exec /bin/sh -c "chmod 777 {} && chown www-data:www-data {}" \;
-find /var/www/html/scripts/ -type f -exec /bin/sh -c "chmod 666 {} && chown www-data:www-data {}" \;
 
 # initialize db_terminal database if necessary
 php /var/www/html/db_terminal_init.php
