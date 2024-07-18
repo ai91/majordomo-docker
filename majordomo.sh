94e357015764b8299286506fdf8470124caeddaf
Anar Ibragimoff <anar.ibragimoff@gmail.com>
2024-07-18 22:56:04 +0200

    add MAJORDOMO_SOFT_RESTORE support

--- majordomo.sh
+++ majordomo.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+TODO on first start also restore base installation* (ignoring existing files)
+3rdparty
+css
+objects
+pChart
+rc
+templates_alt
+.htaccess
+config.php.sample
+favicon.ico
+index.php
+install-linux.sh
+LICENSE
+obj.bat
+README.md
+robots.txt
+
 # prepare list of files to be ignored during restoring content
 # Note: when existing installation is "broken", then ignore DONT_RESTORE_FILES
 #       and restore everything. As a "broken"-indicator - we check absence of cycle.php
@@ -14,11 +31,17 @@ if [ -f "/var/www/html/cycle.php" ]; then
   fi
 fi
 
+if [ -z ${MAJORDOMO_SOFT_RESTORE+x} ]; then
+  RSYNC_OPT="";
+else
+  RSYNC_OPT="--ignore-existing"; 
+fi
+
 # Copy content from /var/www/majordomo into /var/www/html
 echo "Updating installation directories with default majordomo files"
 find /var/www/majordomo/ -type d -exec /bin/sh -c "chmod 777 {} && chown www-data:www-data {}" \;
 find /var/www/majordomo/ -type f -exec /bin/sh -c "chmod 666 {} && chown www-data:www-data {}" \;
-rsync -a --include="config.php" --include="db_terminal_init.php" --exclude-from=/tmp/excludes /var/www/majordomo/ /var/www/html/
+rsync -a $RSYNC_OPT --include="config.php" --include="db_terminal_init.php" --exclude-from=/tmp/excludes /var/www/majordomo/ /var/www/html/
 
 # initialize db_terminal database if necessary
 php /var/www/html/db_terminal_init.php
